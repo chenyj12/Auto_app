a29d65d66e44871350b362ecfba34897aedb58a0
chenyj12 <724253911@qq.com>
2018-11-01 17:37:09 +0800

    修改resource id

--- test_case/test_login.py
+++ test_case/test_login.py
@@ -1,23 +1,26 @@
-from base.capability import driver,NoSuchElementException
+import sys
+import os
+
+curPath = os.path.abspath(os.path.dirname(__file__))
+Path = os.path.split(curPath)[0]
+sys.path.append(Path)
+from base.capability import driver, NoSuchElementException
 
 
-meBtn=driver.find_element_by_id('com.example.homeking.client:id/va')
+meBtn = driver.find_element_by_id('com.example.homeking.client:id/v5')
 
 
 def login():
-    driver.find_element_by_id('com.example.homeking.client:id/o4').click()
+    driver.find_element_by_id('com.example.homeking.client:id/nx').click()
     driver.find_element_by_id('com.example.homeking.client:id/g9').send_keys('15060785807')
     driver.find_element_by_id('com.example.homeking.client:id/g8').send_keys('a147258')
-    driver.find_element_by_id('com.example.homeking.client:id/zw').click()
+    driver.find_element_by_id('com.example.homeking.client:id/zq').click()
+    print('登录完成')
 
 try:
     meBtn.click()
-    id=driver.find_element_by_id('com.example.homeking.client:id/a44').text
-    print('已登录,用户ID为%s'%id)
+    id = driver.find_element_by_id('com.example.homeking.client:id/a40').text
+    print('已登录,用户ID为%s' % id)
 except NoSuchElementException:
-    print('未登录，点击登录')
+    print('点击进行登录')
     login()
-
-
-
-
